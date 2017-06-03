b71da640461149f1378b62292af7085ba2088825
Trent <Trent@fuku.local>
2017-06-03 16:01:49 -0400

    More global definitions & constants. alignment of arguments for readability

--- wrGlobals.h
+++ wrGlobals.h
@@ -1,13 +1,17 @@
 #pragma once
 
-#define HALF24f 4194304.0f
-#define MAX24f 8388607.0f
-#define MAX24f2 16777215.0f
-#define iMAX24f (1 / MAX24f)
+#define HALF24f     4194304.0f
+#define MAX24f      8388607.0f
+#define MAX24f2     16777215.0f
+#define iMAX24f     (1 / MAX24f)
 
-#define MAX24b 0x007FFFFF
-#define MIN24b 0xFF800000
+#define MAX24b2     0x00FFFFFF
+#define MAX24b      0x007FFFFF
+#define HALF24b     0x003FFFFF
+#define MIN24b      0xFF800000
 
-#define nFloor 0.00024 // -72dB
-#define nFloorSub1 (1 - nFloor) // -0.01dB?
-#define nMAX24f 0xFF800000
+#define nFloor      0.00024 // -72dB
+#define nFloorSub1  (1 - nFloor) // -0.01dB?
+#define nMAX24f     0xFF800000
+
+#define WR_PI       3.14159265358979323846264
