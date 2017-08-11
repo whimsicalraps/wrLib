56ba171eccba611b330af8fce7638012251b87fa
Trent <Trent@fuku.local>
2017-08-11 18:09:02 -0400

    reorg. float-defines are now directly cast from integer defines

--- wrGlobals.h
+++ wrGlobals.h
@@ -1,17 +1,20 @@
 #pragma once
 
-#define HALF24f     4194304.0f
-#define MAX24f      8388607.0f
-#define MAX24f2     16777215.0f
-#define iMAX24f     (1 / MAX24f)
-
+// ints
 #define MAX24b2     0x00FFFFFF
 #define MAX24b      0x007FFFFF
 #define HALF24b     0x003FFFFF
 #define MIN24b      0xFF800000
 
+// floats
+#define HALF24f     ((float)HALF24b)
+#define MAX24f      ((float)MAX24b)
+#define MAX24f2     ((float)MAX24b2)
+#define iMAX24f     (1 / MAX24f)
+#define MIN24f      ((float)MIN24b)
+
 #define nFloor      0.00024 // -72dB
 #define nFloorSub1  (1 - nFloor) // -0.01dB?
-#define nMAX24f     0xFF800000
+#define nMAX24f     ((float)0xFF800000)
 
 #define WR_PI       3.14159265358979323846264
