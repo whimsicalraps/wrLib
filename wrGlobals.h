9b6faacc60de89e61ac19077fac24d09a8964c20
Trent <trent.gill@gmail.com>
2017-09-27 19:30:15 -0400

    added 16bit limits & inverse floats for scaling

--- wrGlobals.h
+++ wrGlobals.h
@@ -5,6 +5,7 @@
 #define MAX24b      0x007FFFFF
 #define HALF24b     0x003FFFFF
 #define MIN24b      0xFF800000
+#define MAX16f      0x00007FFF
 
 // floats
 #define HALF24f     ((float)HALF24b)
@@ -12,6 +13,7 @@
 #define MAX24f2     ((float)MAX24b2)
 #define iMAX24f     ((float)(1.0 / MAX24f))
 #define MIN24f      ((float)MIN24b)
+#define iMAX16f     ((float)(1.0 / MAX16f))
 
 #define nFloor      ((float)0.00024) // -72dB
 #define nFloorSub1  ((float)(1.0f - nFloor)) // -0.01dB?
