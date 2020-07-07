496d7ecb9836178d816afba0c8e21f135e273aa3
Trent <trent.gill@gmail.com>
2020-07-06 23:48:45 -0400

    rewrite poly allocator

--- wrPoly.c
+++ wrPoly.c
@@ -10,9 +10,12 @@
 ///////////////////////////////////////
 // helper declarations
 
-static void release( poly_alloc_t* self , uint8_t voice );
-static int8_t assign( poly_alloc_t* self , int16_t note );
-static int8_t steal( poly_alloc_t* self , int16_t note );
+static void release_voice( poly_alloc_t* self, int8_t voice );
+static int8_t find_note( poly_alloc_t* self, int16_t note );
+static int8_t find_item( poly_list_t* self, int8_t item );
+static int8_t take_front( poly_list_t* self );
+static void put_end( poly_list_t* self, int8_t item );
+static void drop_index( poly_list_t* self, uint8_t ix );
 
 
 ///////////////////////////////////////
@@ -25,27 +28,28 @@ poly_alloc_t* poly_init( uint8_t voice_count )
 
     self->voice_count = voice_count;
 
-    self->notes       = malloc(sizeof(int16_t) * voice_count);
+    self->notes = malloc(sizeof(int16_t) * voice_count);
     if( !self->notes ){ printf("poly !self->notes\n"); return NULL; }
-    self->busy_count  = 0;
-    self->busy_list   = malloc(sizeof(uint8_t) * voice_count);
-    if( !self->busy_list ){ printf("poly !self->busy_list\n"); return NULL; }
 
-    self->free_count = voice_count;
-    self->free_list  = malloc(sizeof(uint8_t) * voice_count);
-    if( !self->free_list ){ printf("poly !self->free_list\n"); return NULL; }
+    self->busy.count = 0;
+    self->busy.list  = malloc(sizeof(int8_t) * voice_count);
+    if( !self->busy.list ){ printf("poly !self->busy.list\n"); return NULL; }
+
+    self->free.count = voice_count;
+    self->free.list  = malloc(sizeof(int8_t) * voice_count);
+    if( !self->free.list ){ printf("poly !self->free.list\n"); return NULL; }
     for( uint8_t i=0; i<voice_count; i++ ){
-        self->notes[i]      = RESERVED_PITCH;
-        self->busy_list[i]  = 255;
-        self->free_list[i] = i;
+        self->notes[i]     = RESERVED_PITCH;
+        self->busy.list[i] = -1;
+        self->free.list[i] = i;
     }
     return self;
 }
 
 void poly_deinit( poly_alloc_t* self )
 {
-    free(self->free_list); self->free_list = NULL;
-    free(self->busy_list); self->busy_list = NULL;
+    free(self->free.list); self->free.list = NULL;
+    free(self->busy.list); self->busy.list = NULL;
     free(self->notes); self->notes = NULL;
     free(self); self = NULL;
 }
@@ -54,98 +58,101 @@ void poly_deinit( poly_alloc_t* self )
 ///////////////////////////////////////
 // public interface
 
-int8_t poly_assign_note( poly_alloc_t* self
-                       , int16_t       note
-                       )
+int8_t poly_assign_note( poly_alloc_t* self, int16_t note )
 {
     if( note == RESERVED_PITCH ){ return -1; } // invalid pitch
 
-    // TODO search for note w same pitch
+    int8_t voice = take_front( (self->free.count)
+                                    ? &self->free
+                                    : &self->busy );
+    put_end( &self->busy, voice );
 
-    return (self->free_count)
-            ? assign( self, note )
-            : steal( self, note );
+    self->notes[voice] = note; // save pitch
+
+    return voice;
 }
 
-int8_t poly_kill_note( poly_alloc_t* self
-                     , int16_t       note
-                     )
+void poly_assign_voice( poly_alloc_t* self, int8_t voice, int16_t note )
+{
+    if( voice < 0 || voice >= self->voice_count ){ return; } // invalid voice
+    if( note == RESERVED_PITCH ){ return; } // invalid pitch
+
+    release_voice( self, voice );
+    put_end( &self->busy, voice );
+
+    self->notes[voice] = note; // save pitch
+}
+
+
+int8_t poly_kill_note( poly_alloc_t* self, int16_t note )
 {
     if( note == RESERVED_PITCH ){ return -1; } // invalid pitch
 
-    // search for match in self->notes
-    for( uint8_t i=0; i < self->voice_count; i++){
-        if(note == self->notes[i]){ // found, so kill this
-            release( self, i);
-            return i;
-        }
-    }
-    return -1;
+    int16_t voice = find_note( self, note );
+    poly_kill_voice( self, voice );
+
+    return voice;
+}
+
+
+void poly_kill_voice( poly_alloc_t* self, int8_t voice )
+{
+    if( voice < 0 || voice >= self->voice_count ){ return; }
+
+    release_voice( self, voice );
+    put_end( &self->free, voice );
+
+    self->notes[voice] = RESERVED_PITCH; // forget pitch
 }
 
 ///////////////////////////////////////
 // helper fns
 
-static void release( poly_alloc_t* self
-                   , uint8_t       voice
-                   )
-{
-    self->notes[voice] = RESERVED_PITCH; // forget pitch
-    --self->busy_count; // decrement busy length
-
-    uint8_t i = 0; // start search from oldest voice in the list
-    while( voice != self->busy_list[i] ){ i++; } // find voice in aged-list
-    // overwrite released voice & shift all newer voices forward in the list
-    for(i; i < self->busy_count; i++){
-        self->busy_list[i] = self->busy_list[i+1];
+static void release_voice( poly_alloc_t* self, int8_t voice ){
+    int8_t ix = find_item( &self->busy, voice );
+    if( ix != -1 ){ // found in busy
+        drop_index( &self->busy, ix );
+    } else {
+        ix = find_item( &self->free, voice ); // found in free
+        drop_index( &self->free, ix );
     }
-
-    // TODO this is same as middle of assign/steal. abstract and pass list?
-    // add to end of free list
-    self->free_list[self->free_count] = voice;
-    self->free_count++;
 }
 
-// TODO refactor as the only difference bw assign & steal is which sorted-list
-    // is the voice taken from. just pass the list as an object to a single fn
-static int8_t assign( poly_alloc_t* self
-                    , int16_t       note
-                    )
-{
-    int8_t voice = self->free_list[0]; // steal front of the free list
-    self->free_count--;
-
-    // shift free list forward
-    for( uint8_t i=0; i < self->free_count; i++ ){
-        self->free_list[i] = self->free_list[i+1];
+static int8_t find_note( poly_alloc_t* self, int16_t note ){
+    for( uint8_t i=0; i < self->voice_count; i++ ){
+        if( note == self->notes[i] ){ return i; }
     }
-
-    // add to end of busy list
-    self->busy_list[self->busy_count] = voice;
-    self->busy_count++;
-
-    self->notes[voice] = note; // save pitch
-
-    return voice;
+    return -1;
 }
 
-static int8_t steal( poly_alloc_t* self
-                   , int16_t       note
-                   )
-{
-    int8_t voice = self->busy_list[0]; // steal front of busy list (oldest)
-    self->busy_count--;
-
-    // shift busy list forward
-    for( uint8_t i=0; i < self->busy_count; i++ ){
-        self->busy_list[i] = self->busy_list[i+1];
+static int8_t find_item( poly_list_t* self, int8_t item ){
+    for( uint8_t i=0; i < self->count; i++ ){
+        if( item == self->list[i] ){ return i; }
     }
+    return -1;
+}
 
-    // add to end of busy list
-    self->busy_list[self->busy_count] = voice;
-    self->busy_count++;
+static int8_t take_front( poly_list_t* self )
+{
+    int8_t front = self->list[0];
+    self->count--;
 
-    self->notes[voice] = note; // save pitch
+    // shift list forward
+    for( uint8_t i=0; i < self->count; i++ ){
+        self->list[i] = self->list[i+1];
+    }
+    return front;
+}
 
-    return voice;
+static void put_end( poly_list_t* self, int8_t item )
+{
+    self->list[self->count] = item;
+    self->count++;
+}
+
+static void drop_index( poly_list_t* self, uint8_t ix ){
+    --self->count;
+    for(; ix < self->count; ix++){
+        self->list[ix] = self->list[ix+1];
+    }
 }
--- wrPoly.h
+++ wrPoly.h
@@ -15,23 +15,28 @@
  *       trigger in same channel in case still sustaining.
  */
 
+typedef struct{
+    uint8_t  count;
+    int8_t* list;
+} poly_list_t;
+
 typedef struct poly_alloc{
     uint8_t  voice_count;
 
-    int16_t* notes;
+    int16_t*    notes; // list of notes for the voices
+    poly_list_t busy; // list of busy notes, (oldest..newest)
+    poly_list_t free; // list of free notes, (oldest..newest)
+    //uint8_t  busy_count;
+    //uint8_t* busy_list;
 
-    uint8_t  busy_count;
-    uint8_t* busy_list;
-
-    uint8_t  free_count;
-    uint8_t* free_list;
+    //uint8_t  free_count;
+    //uint8_t* free_list;
 } poly_alloc_t;
 
 poly_alloc_t* poly_init( uint8_t voice_count );
 void poly_deinit( poly_alloc_t* self );
-int8_t poly_assign_note( poly_alloc_t* self
-                       , int16_t       note
-                       );
-int8_t poly_kill_note( poly_alloc_t* self
-                     , int16_t       note
-                     );
+
+int8_t poly_assign_note( poly_alloc_t* self , int16_t note );
+void poly_assign_voice( poly_alloc_t* self, int8_t voice, int16_t note );
+int8_t poly_kill_note( poly_alloc_t* self, int16_t note );
+void poly_kill_voice( poly_alloc_t* self, int8_t voice );
