916e2dedfe6b3d8f7e87f41162e630100e4fba32
= <=>
2021-03-05 12:21:14 -0800

    update comp settings for w/syn. need to make it config-able once i understand how it works

--- dsp/wrCompress.c
+++ dsp/wrCompress.c
@@ -11,11 +11,12 @@ compress_t* compress_init( void )
 
     self->sidechain = 1.0; // last gain multiplier
     self->lp1 = lp1_a_init();
-    lp1_a_set_coeff( self->lp1, 0.0001, 0.00005 );
+    lp1_a_set_coeff( self->lp1, 0.001, 0.0001 );
 
     return self;
 }
 
+// static int THROTTLE = 4800;
 float* compress_step_v(compress_t* self, float* io, int size)
 {
     float* b = io;
@@ -26,18 +27,24 @@ float* compress_step_v(compress_t* self, float* io, int size)
         // absolute val
         sc = fabsf(*b);
         // threshold
-        sc -= 0.7;
+        sc -= 0.51;
         sc = (sc < 0.0) ? 0.0 : sc; // ensure no gain boost
         // ratio multiplier
         // TODO soft-knee non-linearity
-        sc = 0.45*sc; // just slightly below clipping
-        sc = (sc > 0.87) ? 0.87 : sc; // make sure we don't apply inverse gain
+        // sc = 0.45*sc; // just slightly below clipping
+        sc = 2.5*sc;
+        float PRE = sc;
+        // sc = (sc > 0.55) ? 0.55 : sc; // make sure we don't apply inverse gain
         // dual-k 1pole lowpass
-        sc = lp1_a_step( self->lp1, sc );
+        sc = fminf(1.0, lp1_a_step( self->lp1, sc ));
+        float SMOOTH = sc;
         // convert from gain-reduction to gain-level
-        sc = 1.0 - sc;
-        // sc = sc*sc; // scales to apply closer to an audio-taper reduction
+        sc = 1.0 - (sc*0.72); // reduces limit to 0.5x gain
+        sc = fmaxf(0.5,sc);
+        // if( --THROTTLE <= 0){ THROTTLE = 4800;
+            // printf("%f %f %f\n",(double)PRE, (double)SMOOTH, (double)sc);}
         // apply gain-reduction
+        // *b++ *= sc*0.5; // TESTING. reduces level by 50% to avoid clip
         *b++ *= sc;
         // save servo feedback with threshold applied
         sc = sc + 0.0; // TODO threshold
